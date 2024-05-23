08d9bc7ae8234df0d99a38adf8eb268beb9b609b
datvo5566 <146400229+datvo5566@users.noreply.github.com>
2024-05-23 21:29:46 +0700

    Add Modelview

---
+++ Sem3Final/Models/ModelsView/AnswerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccpSem3.Models.ModeView
+{
+    public class AnswerView
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public Nullable<int> id_question { get; set; }
+        public bool Is_correct1 { get; set; }
+        public Nullable<int> is_correct { get; set; }
+        public Nullable<System.DateTime> created_at { get; set; }
+        public Nullable<System.DateTime> updated_at { get; set; }
+    }
+}
