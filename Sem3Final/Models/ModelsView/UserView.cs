746c4a46c46c58f9a29b44e542d421e2f3723039
KiwnNguyen <128109332+KiwnNguyen@users.noreply.github.com>
2024-05-25 17:28:51 +0700

    Bổ sung những file cần thiết trong models folder

---
+++ Sem3Final/Models/ModelsView/UserView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sem3Final.Models.ModelsView
+{
+    public class UserView
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string pass_word { get; set; }
+        public string fullname { get; set; }
+        public Nullable<int> status { get; set; }
+        public string images { get; set; }
+        public Nullable<System.DateTime> created_at { get; set; }
+        public Nullable<System.DateTime> updated_at { get; set; }
+    }
+}
