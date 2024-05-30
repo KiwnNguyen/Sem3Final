8e256596380c8e83e3b3a950f8a13af7aa09b428
datvo5566 <146400229+datvo5566@users.noreply.github.com>
2024-05-30 17:56:20 +0700

    thêm model QuesAndExam

---
+++ Sem3Final/Models/ModelsView/ModelJoin/QuesAndExam.cs
@@ -0,0 +1,15 @@
+﻿using Sem3Final.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sem3Final.Models.ModelsView.ModelJoin
+{
+    public class QuesAndExam
+    {
+        public Examination examination { get; set; }
+        public QuestionOfExamination question_exam { get; set; }
+        public Question question { get; set; }
+    }
+}
