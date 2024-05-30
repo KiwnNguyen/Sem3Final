a2c5bcfd5db308de9079d753a4ba83f14a3421f3
datvo5566 <146400229+datvo5566@users.noreply.github.com>
2024-05-30 18:45:51 +0700

    Thêm phương thức mã hóa password và email

---
+++ Sem3Final/Models/Email/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+
+namespace Sem3Final.Models.Email
+{
+    public class EmailSender
+    {
+        private static EmailSender instance;
+        private EmailSender() { }
+        public static EmailSender Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EmailSender();
+                }
+                return instance;
+            }
+        }
+        public void SendEmail(string from, string to, string subject, string body)
+        {
+            //Khởi tạo email 
+            MailMessage mail = new MailMessage(from, to, subject, body);
+            mail.IsBodyHtml = true;//khai báo nội dụng email là html
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+
+            smtpClient.Credentials = new NetworkCredential(from, "bxtkvcybvgcjcdjp");
+            smtpClient.EnableSsl = true;
+            try
+            {
+                smtpClient.Send(mail);
+                Console.WriteLine("Email send success");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to send email. Error message: " + e.Message);
+            }
+
+        }
+    }
+}
---
+++ Sem3Final/Models/Encryption/PasswordHasher1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sem3Final.Models.Encryption
+{
+    public class PasswordHasher1
+    {
+        public static string EncodePasswordToBase64(string password)
+        {
+            try
+            {
+                if (password != null)
+                {
+                    byte[] encData_byte = new byte[password.Length];
+                    encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                    string encodeData = Convert.ToBase64String(encData_byte);
+                    return encodeData;
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return null;
+        }
+
+
+        public static string DecodeFrom64(string Encodepassword)
+        {
+            try
+            {
+                if (Encodepassword != null)
+                {
+                    System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                    System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                    byte[] tocode_byte = Convert.FromBase64String(Encodepassword);
+                    int charCount = utf8Decode.GetCharCount(tocode_byte, 0, tocode_byte.Length);
+                    char[] decoded_char = new char[charCount];
+                    utf8Decode.GetChars(tocode_byte, 0, tocode_byte.Length, decoded_char, 0);
+                    string result = new string(decoded_char);
+                    return result;
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return null;
+        }
+    }
+}
